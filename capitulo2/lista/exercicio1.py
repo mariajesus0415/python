52ebd4673676df24d435b1003045598d929f61c9
endrius <endrius.pontes@escola.pr.gov.br>
2025-05-23 11:59:10 -0300

    exercicio1

---
+++ capitulo2/lista/exercicio1.py
@@ -0,0 +1,22 @@
+    #definindo os numeros
+num1 = int(input("Digite o primeiro número interiro:"))
+num2 = int(input("Digite o segundo numero inteiro:"))
+
+    # Realizando as operações
+adicao = (num1  + num2)
+subitrair = (num1 - num2)
+multiplicacao = (num1 * num2)
+divisao = (num1 / num2)
+
+print(adicao)
+print(subitrair)
+print(multiplicacao)
+print(divisao)
+
+
+
+
+
+
+
+
